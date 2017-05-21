cc61f37a1ea885548a4b3d3a95b2533430f4f549
Mike Hewitt <matroxmike@icloud.com>
2017-05-21 14:34:59 +1000

    reviving old project

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject javaxSwing "1.0.2-SNAPSHOT"
+(defproject javaxSwing "1.0.3-SNAPSHOT"
   :description "Using javax swing - worked OK in clojure repl a.o.t. lein repl"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   
-  :main javaxSwing.core)
+  :main javaxSwing.core
+  :aot
+  :all
+  )
